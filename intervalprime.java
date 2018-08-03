c4c0e8daa5469d298a627ba09b47ec0f7084df63
shaziabarkat5 <38329467+shaziabarkat5@users.noreply.github.com>
2018-08-03 12:20:16 +0530

    Add files via upload

--- intervalprime.java
+++ intervalprime.java
@@ -1,25 +1,30 @@
-import java.util.*;
 public class palin {
 
 	public static void main(String[] args) {
-		int a1,a2=0;
+		int a1,a2=0,flag =0,i,j;
 		System.out.println("enter the intervals :");
 		Scanner a = new Scanner(System.in);
 		a1 = a.nextInt();
 		a2= a.nextInt();
 		a.close();
-		for(int i=a1+1;i<a2;i++)
-		{
-			for(int j=2;i<a2;j++)
-			{
-				if(i%j!=0)
-				{
-					System.out.println(i);
-					break;
-				}
-				
-		   }
-		
-	   }
+		for(i = a1+1; i < a2; i++)
+        {
+            for( j = 2; j < i; j++)
+            {
+                if(i % j == 0)
+                {
+                    flag = 0;
+                    break;
+                }
+                else
+                {
+                    flag = 1;
+                }
+            }
+            if(flag == 1)
+            {
+                System.out.println(i);
+            }
+
 	}
-}
+}}
